f08d0f7c4d4d0e380fab5d9faba91c9c24925823
Benjamin Nomba <bnomba0704@gmail.com>
2023-02-09 16:35:45 -0800

    added comp

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="back mx-100 h-screen bg-black">
         <NavBar/>
-        
     </div>
+    <Products/>
 </template>
 
 <script>
 import NavBar from './NavBar.vue';
+import Products from './Products.vue';
     export default {
         name: 'Home',
         components: {
-            NavBar
+            NavBar,
+            Products
         }
     }
 </script>
