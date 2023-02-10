d55e5da04dee218c98f33eb17d7fb3d768cd246f
Benjamin Nomba <bnomba0704@gmail.com>
2023-02-09 16:19:22 -0800

    new file:   .gitignore 	new file:   README.md 	new file:   babel.config.js 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   src/App.vue 	new file:   src/assets/index.css 	new file:   src/components/Home.vue 	new file:   src/components/NavBar.vue 	new file:   src/images/first.png 	new file:   src/images/fruits.jpg 	new file:   "src/images/pexels-adonyi-g\303\241bor-1414651.jpg" 	new file:   src/main.js 	new file:   tailwind.config.js 	new file:   vue.config.js

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="back mx-100 h-screen bg-black">
+        <NavBar/>
+        
+    </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+    export default {
+        name: 'Home',
+        components: {
+            NavBar
+        }
+    }
+</script>
+
+<style>
+
+</style>
