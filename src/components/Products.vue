b86930761526ec964d938a2d1da90c540a58615d
Benjamin Nomba <bnomba0704@gmail.com>
2023-02-15 10:00:19 -0800

    modelsetup

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,46 +1,101 @@
 <template>
     <!-- Product container -->
     <div>
-        <div>
-            <main class="wrapper">
-                <h1>Products</h1>
+        <div class="mx-auto ">
+            <main class="flex flex-col justify-evenly self-center justify-center wrapper h-screen">
+                <h1 class="text-center">Products</h1>
             
-                <div class="card-container">
-                    <div class="card">
-                        <div class="card-title">
+                <div class="card-container h-4/6 flex justify-evenly" >
+                    <div class="flex flex-col justify-between w-1/4 h-full card bg-zinc-400">
+                        <div class="card-title text-center py-5">
                             Carrots
                         </div>
-                        <div class="card-body">
-                            <i class="icofont-10x icofont-carrot"></i>
-                            <form>
-                                <div class="row">
-                                    <div class="cell">
-                                        <label>Type:</label>
-                                    </div>
-                                    <div class="cell">
-                                        <em>Vegetable</em>
-                                    </div>
+                        <div class="relative card-body">
+                            <div class=" mb-5 flex justify-center">
+
+                                <img src="../images/carrot.png" alt="carrot" width="150px">
+                            </div>
+                            <form class="flex justify-evenly space-x-12">
+                                <div class="label space-y-2">
+                                    <div>Type:</div>
+                                    <div>Price:</div>
+                                    <div>Quantity:</div>
                                 </div>
-                                <div class="row">
-                                    <div class="cell">
-                                        <label>Price:</label>
-                                    </div>
+
+                                <div class="results space-y-2">
+                                    <em>Vegetable</em>
                                     <div class="cell">
                                         $4.82
                                     </div>
+                                    <input type="number" value="0">
                                 </div>
-                                <div class="row">
-                                    <div class="cell">
-                                        <label>Quantity:</label>
-                                    </div>
-                                    <div class="cell">
-                                        <input type="number" value="0">
-                                    </div>
-                                </div>
+                                
                             </form>
                         </div>
-                        <div class="card-footer">
-                            <button class="btn btn-light">Add to cart</button>
+                        <div class="card-footer text-center p-3">
+                            <button class="btn ">Add to cart</button>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-between w-1/4 h-full card bg-zinc-400">
+                        <div class="card-title text-center py-5">
+                            Carrots
+                        </div>
+                        <div class="relative card-body">
+                            <div class=" mb-5 flex justify-center">
+                    
+                                <img src="../images/carrot.png" alt="carrot" width="150px">
+                            </div>
+                            <form class="flex justify-evenly space-x-12">
+                                <div class="label space-y-2">
+                                    <div>Type:</div>
+                                    <div>Price:</div>
+                                    <div>Quantity:</div>
+                                </div>
+                    
+                                <div class="results space-y-2">
+                                    <em>Vegetable</em>
+                                    <div class="cell">
+                                        $4.82
+                                    </div>
+                                    <input type="number" value="0">
+                                </div>
+                    
+                            </form>
+                        </div>
+                        <div class="card-footer text-center p-3">
+                            <button class="btn ">Add to cart</button>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col justify-between w-1/4 h-full card bg-zinc-400">
+                        <div class="card-title text-center py-5">
+                            Carrots
+                        </div>
+                        <div class="relative card-body">
+                            <div class=" mb-5 flex justify-center">
+                    
+                                <img src="../images/carrot.png" alt="carrot" width="150px">
+                            </div>
+                            <form class="flex justify-evenly space-x-12">
+                                <div class="label space-y-2">
+                                    <div>Type:</div>
+                                    <div>Price:</div>
+                                    <div>Quantity:</div>
+                                </div>
+                    
+                                <div class="results space-y-2">
+                                    <em>Vegetable</em>
+                                    <div class="cell">
+                                        $4.82
+                                    </div>
+                                    <input type="number" value="0">
+                                </div>
+                    
+                            </form>
+                        </div>
+                        <div class="card-footer text-center p-3">
+                            <button class="btn ">Add to cart</button>
                         </div>
                     </div>
                 </div>    
@@ -50,6 +105,8 @@
 </template>
 
 <script>
+
+
 export default{
     name: 'Products'
 }
